0d580358e6762aa959691d74e8f571dbc7d25019
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-07-14 14:05:57 +0530

    setup supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+import supabase from "./db/supabase";
+
 
 function App() {
+  const [instruments, setInstruments] = useState([]);
+
+  useEffect(() => {
+    getInstruments();
+  }, []);
+
+  async function getInstruments() {
+    const { data } = await supabase.from("urls").select();
+    console.log(data)
+    setInstruments(data);
+  }
 
   return (
-    <>
-      <h1 className='text-red-400'>Hellow</h1>
-    </>
-  )
+    <ul>
+      {instruments.map((instrument) => (
+        <li key={instrument.id}>{
+          <pre>
+            {JSON.stringify(instrument, null, 2)}
+          </pre>
+        }</li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
