0faceac9b43d7386ed910909d8de8c9bcdadefb0
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-07-21 17:51:14 +0530

    added background lines to Home comp.

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -6,8 +6,8 @@ const AppLayout = () => {
   return (
     <main className="bg-background h-screen max-h-screen">
       <div className="container h-full flex flex-col">
-        <Toaster/>
-        <Header />
+        <Toaster />
+          <Header />
           <Outlet />
       </div>
     </main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Section from "@/components/Section";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "@/store/global-state";
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -10,8 +10,8 @@ const Home = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  const data = new FormData(e.currentTarget);
-  const url = data.get("url") as string;
+    const data = new FormData(e.currentTarget);
+    const url = data.get("url") as string;
 
     if (user) {
       navigate(`/dashboard?createNew=${url}`);
@@ -20,26 +20,28 @@ const Home = () => {
     }
   };
   return (
-    <Section className="gap-12 justify-center items-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-semibold text-foreground mb-2">
-          Shorten Your URL
-        </h1>
-        <p className="text-muted-foreground max-w-8/12 mx-auto">
-          Enter your long URL below and we’ll generate a shorter one for you.
-        </p>
-      </div>
-      <form onSubmit={handleSubmit} className="flex gap-2 w-6/12">
-        <Input
-          type="url"
-          placeholder="Enter your very long URL"
-          name="url"
-          className="flex-1 placeholder:text-xs"
-          required
-        />
-        <Button type="submit">Shorten</Button>
-      </form>
-    </Section>
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+      <>
+        <div className="text-center mb-8">
+          <h1 className="text-5xl font-semibold text-foreground mb-2">
+            Shorten Your URL
+          </h1>
+          <p className="text-muted-foreground max-w-8/12 mx-auto">
+            Enter your long URL below and we’ll generate a shorter one for you.
+          </p>
+        </div>
+        <form onSubmit={handleSubmit} className="flex gap-2 w-6/12 z-40">
+          <Input
+            type="url"
+            placeholder="Enter your very long URL"
+            name="url"
+            className="flex-1 placeholder:text-xs"
+            required
+          />
+          <Button type="submit">Shorten</Button>
+        </form>
+      </>
+    </BackgroundLines>
   );
 };
 
